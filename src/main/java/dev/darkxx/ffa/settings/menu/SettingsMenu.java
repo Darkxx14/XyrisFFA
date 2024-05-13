8128d30f8edfc34f913b5569319628baf560f975
Darkxx14 <packs.gg1@gmail.com>
2024-05-13 16:34:21 +0530

    (:

--- src/main/java/dev/darkxx/ffa/settings/menu/SettingsMenu.java
+++ src/main/java/dev/darkxx/ffa/settings/menu/SettingsMenu.java
@@ -12,9 +12,7 @@ import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static dev.darkxx.ffa.Main.formatColors;
 
