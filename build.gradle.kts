7dcb1d49c91b0a404dcb6ad61f39dd1a0b2898e5
Darkxx14 <packs.gg1@gmail.com>
2024-05-20 22:30:03 +0530

    fixed some issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,43 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     `java-library`
     `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
+
+group = "dev.darkxx"
+version = "1.0.0"
+description = "FFA"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenLocal()
     maven {
         url = uri("https://repo.bytecode.space/repository/maven-public/")
     }
-
     maven {
         url = uri("https://repo.papermc.io/repository/maven-public/")
     }
-
     maven {
         url = uri("https://oss.sonatype.org/content/groups/public/")
     }
-
     maven {
         url = uri("https://repo.dmulloy2.net/nexus/repository/public/")
     }
-
     maven {
         url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     }
-
     maven {
         url = uri("https://maven.enginehub.org/repo/")
     }
-
     maven {
         url = uri("https://repo.maven.apache.org/maven2/")
     }
 }
 
 dependencies {
-    api("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.zaxxer:HikariCP:5.1.0")
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("ca.bkaw:paper-nms:1.20.4-SNAPSHOT")
     compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
@@ -45,23 +47,29 @@ dependencies {
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit:2.7.0")
 }
 
-group = "dev.darkxx"
-version = "1.0.0"
-description = "FFA"
-java.sourceCompatibility = JavaVersion.VERSION_17
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
 
 publishing {
     publications.create<MavenPublication>("maven") {
         from(components["java"])
+        artifact(tasks["shadowJar"])
     }
 }
 
-tasks.withType<JavaCompile>() {
+tasks.shadowJar {
+    minimize()
+    archiveFileName.set("${project.name}-${project.version}.jar")
+    relocate("com.zaxxer:HikariCP", "dev.darkxx.ffa.shaded.com.zaxxer:HikariCP")
+}
+
+tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
 project.buildDir = File(rootDir, "output")
 
-tasks.withType<Javadoc>() {
+tasks.withType<Javadoc> {
     options.encoding = "UTF-8"
 }
