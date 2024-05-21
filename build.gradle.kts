addd58c6f50d93628abb253c3768c979359f4586
Darkxx14 <packs.gg1@gmail.com>
2024-05-21 08:35:54 +0530

    update build.gradle.kts & plugin.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,9 @@
 plugins {
     `java-library`
     `maven-publish`
-}
-
-repositories {
-    mavenLocal()
-    maven {
-        url = uri("https://repo.bytecode.space/repository/maven-public/")
-    }
-
-    maven {
-        url = uri("https://repo.papermc.io/repository/maven-public/")
-    }
-
-    maven {
-        url = uri("https://oss.sonatype.org/content/groups/public/")
-    }
-
-    maven {
-        url = uri("https://repo.dmulloy2.net/nexus/repository/public/")
-    }
-
-    maven {
-        url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-    }
-
-    maven {
-        url = uri("https://maven.enginehub.org/repo/")
-    }
-
-    maven {
-        url = uri("https://repo.maven.apache.org/maven2/")
-    }
-}
-
-dependencies {
-    api("com.zaxxer:HikariCP:5.1.0")
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
-    compileOnly("ca.bkaw:paper-nms:1.20.4-SNAPSHOT")
-    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
-    compileOnly("me.clip:placeholderapi:2.11.5")
-    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.8")
-    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:2.7.0")
-    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit:2.7.0")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("io.papermc.paperweight.userdev") version "1.6.3"
+    id("xyz.jpenilla.run-paper") version "2.2.4"
 }
 
 group = "dev.darkxx"
@@ -50,18 +11,62 @@ version = "1.0.0"
 description = "FFA"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
-publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+    maven {
+        url = uri("https://repo.dmulloy2.net/nexus/repository/public/")
+    }
+    maven {
+        url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    }
+    maven {
+        url = uri("https://maven.enginehub.org/repo/")
+    }
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
     }
 }
 
-tasks.withType<JavaCompile>() {
+dependencies {
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
+    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
+    compileOnly("me.clip:placeholderapi:2.11.5")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.8")
+    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:2.7.0")
+    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit:2.7.0")
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+        artifact(tasks["shadowJar"])
+    }
+}
+
+tasks.shadowJar {
+    minimize()
+    archiveFileName.set("${project.name}-${project.version}.jar")
+    relocate("com.zaxxer:HikariCP", "dev.darkxx.ffa.shaded.com.zaxxer:HikariCP")
+}
+
+tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
 project.buildDir = File(rootDir, "output")
 
-tasks.withType<Javadoc>() {
+tasks.withType<Javadoc> {
     options.encoding = "UTF-8"
 }
